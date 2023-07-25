b475f3c5840f3f3491ad9dc317b3e8ee6d6127df
Diego <diegosccpinto@gmail.com>
2023-07-25 14:11:15 -0300

    Iniciando no css

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
-
-
-API_KEY = dotenv.API_KEY;
+const API_KEY = "c2318667c3843865fe1a4900b7859bec";
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`
+const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+const SEARCH_URL = `${BASE_URL}/search/movie?${API_KEY}`;
 
 
 function closeNav(){
