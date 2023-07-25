3dbf587d14e759a0bdae523cb705ae89ca563328
Diego <diegosccpinto@gmail.com>
2023-07-25 15:00:10 -0300

    Função setGenre()
     * Cria e exibe uma lista de tags correspondentes a diferentes gêneros de filmes

--- script.js
+++ script.js
@@ -103,6 +103,37 @@ window.addEventListener("load", () => {
   loader.style.display = "none";
 });
 
+let selectedGenre = [];
+
+function setGenre() {
+  tagsEl.innerHTML = "";
+  genres.forEach((genre) => {
+    const t = document.createElement("div");
+    t.classList.add("tag");
+    t.id = genre.id;
+    t.innerText = genre.name;
+    t.addEventListener("click", () => {
+      if (selectedGenre.length == 0) {
+        selectedGenre.push(genre.id);
+      } else {
+        if (selectedGenre.includes(genre.id)) {
+          selectedGenre.forEach((id, idx) => {
+            if (id == genre.id) {
+              selectedGenre.splice(idx, 1);
+            }
+          });
+        } else {
+          selectedGenre.push(genre.id);
+        }
+      }
+      console.log(selectedGenre);
+      getMovies(API_URL + "&with_genres=" + encodeURI(selectedGenre.join(",")));
+      highlightSelection();
+    });
+    tagsEl.append(t);
+  });
+}
+
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
