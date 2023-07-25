1f05b3a0aecb2500eff5ac9236858c19fe145747
Diego <diegosccpinto@gmail.com>
2023-07-25 19:36:47 -0300

    Create function getMovies, showMovies e getColor
    - Função getMovies:  Obtém os dados dos filmes da API do TMDB
    - Função showMovies: Exibe os filmes na página da web com base nos dados fornecidos.
    - Função getColor: Retorna a cor correspondente à classificação de um filme com base na pontuação

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const API_KEY = "c2318667c3843865fe1a4900b7859bec";
+const API_KEY = "api_key=c2318667c3843865fe1a4900b7859bec";
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
+const API_URL = `${BASE_URL}/discover/movie?language=pt-BR&sort_by=popularity.desc&${API_KEY}`;
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 const SEARCH_URL = `${BASE_URL}/search/movie?${API_KEY}`;
 
@@ -14,7 +14,7 @@ const genres = [
     name: "Adventure",
   },
   {
-    id: 16,
+    id: 16, 
     name: "Animation",
   },
   {
@@ -139,9 +139,111 @@ function setGenre() {
     tagsEl.append(t);
   });
 }
+getMovies(API_URL);
+function getMovies(url) {
+  /**
+   * Obtém os dados dos filmes da API do TMDB com base na URL fornecida e exibe os filmes na página da web.
+   *
+   * @param {string} url - A URL da API para obter os dados dos filmes.
+   * @returns {void} - Esta função não retorna nada. Ela atualiza a página da web com os dados dos filmes obtidos.
+   */
+  lastUrl = url;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.results);
+      if (data.results.length !== 0) {
+        showMovies(data.results);
+        currentPage = data.page;
+        nextPage = currentPage + 1;
+        prevPage = currentPage - 1;
+        totalPages = data.total_pages;
 
+        current.innerText = currentPage;
 
+        if (currentPage <= 1) {
+          prev.classList.add("disabled");
+          next.classList.remove("disabled");
+        } else if (currentPage >= totalPages) {
+          prev.classList.remove("disabled");
+          next.classList.add("disabled");
+        } else {
+          prev.classList.remove("disabled");
+          next.classList.remove("disabled");
+        }
+
+        tagsEl.scrollIntoView({ behavior: "smooth" });
+      } else {
+        main.innerHTML = `<h1 class="no-results">No Results Found</h1>`;
+      }
+    });
+}
+
+function showMovies(data) {
+  /**
+   * Exibe os filmes na página da web com base nos dados fornecidos.
+   *
+   * @param {array} data - Os dados dos filmes a serem exibidos na página. Deve ser uma lista de filme.
+   * @returns {void} - Esta função não retorna nada. Ela atualiza a página da web com os filmes exibidos.
+   */
+  main.innerHTML = "";
+
+  data.forEach((movie) => {
+    const { title, poster_path, vote_average, overview, id } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+             <img src="${
+               poster_path
+                 ? IMAGE_URL + poster_path
+                 : "http://via.placeholder.com/1080x1580"
+             }" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+
+                <h3>Sinopse</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Saiba mais</button
+            </div>
+        
+        `;
+
+    main.appendChild(movieEl);
+
+    document.getElementById(id).addEventListener("click", () => {
+      console.log(id);
+      openNav(movie);
+    });
+  });
+}
+
+function getColor(vote) {
+  /**
+   * Retorna a cor correspondente à classificação de um filme com base na pontuação (voto) fornecida.
+   *
+   * @param {number} vote - A pontuação (voto) do filme, que deve estar em uma escala de 0 a 10.
+   * @returns {string} - A cor correspondente à classificação do filme: "green" para alta classificação, "orange" para média classificação e "red" para baixa classificação.
+   */
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 
 function closeNav() {
+/**
+ * Fecha a navegação (overlay) que exibe trailers de filmes na página.
+ * 
+ * @returns {void} - Esta função não retorna nada. Ela fecha a navegação ao ajustar a largura para "0%".
+ */
   document.getElementById("myNav").style.width = "0%";
 }
